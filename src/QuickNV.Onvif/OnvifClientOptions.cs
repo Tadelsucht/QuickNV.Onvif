1ac9149fd7fbb048f9f4c372bd8c5b2f563c7fb1
张鹏 <scbeta@qq.com>
2026-05-29 21:06:11 +0800

    修复OnvifClientOptions反序列化时可能出错的问题

--- src/QuickNV.Onvif/OnvifClientOptions.cs
+++ src/QuickNV.Onvif/OnvifClientOptions.cs
@@ -1,21 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.ServiceModel;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ServiceModel;
+using System.Text.Json.Serialization;
 
 namespace QuickNV.Onvif
 {
+    [JsonSerializable(typeof(OnvifClientOptions))]
+    [JsonSourceGenerationOptions(
+DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+WriteIndented = true)]
+    public partial class OnvifClientOptionsSerializerContext : JsonSerializerContext { }
+
     public class OnvifClientOptions
     {
         public string Scheme { get; set; } = "http";
         public string Host { get; set; }
+        [JsonNumberHandling(JsonNumberHandling.AllowReadingFromString)]
         public int Port { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
+        [JsonConverter(typeof(JsonStringEnumConverter<HttpClientCredentialType>))]
         public HttpClientCredentialType ClientCredentialType { get; set; } = HttpClientCredentialType.Digest;
+        [JsonNumberHandling(JsonNumberHandling.AllowReadingFromString)]
         public int RtspPort { get; set; }
+        [JsonNumberHandling(JsonNumberHandling.AllowReadingFromString)]
         public int SnapshotPort { get; set; }
     }
 }
